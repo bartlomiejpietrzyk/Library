3feb47368c13e4389468fb591cc881b2087c75b9
Bartłomiej Pietrzyk <bapietrzyk@gmail.com>
2019-10-23 14:04:42 +0200

    Book class with builder pattern

---
+++ src/main/java/pl/bartlomiejpietrzyk/library/Book.java
@@ -0,0 +1,77 @@
+package pl.bartlomiejpietrzyk.library;
+
+public class Book {
+    private static int count = 0;
+    private final Integer id = ++count;
+    private final String title;
+    private final String author;
+    private final Integer year;
+    private String lentBy = "";
+
+    public Book(Builder builder) {
+        this.title = builder.title;
+        this.author = builder.author;
+        this.year = builder.year;
+    }
+
+    static class Builder {
+        private String title = null;
+        private String author = null;
+        private Integer year = null;
+
+        Builder title(String title) {
+            this.title = title;
+            return this;
+        }
+
+        Builder author(String author) {
+            this.author = author;
+            return this;
+        }
+
+        Builder year(Integer year) {
+            this.year = year;
+            return this;
+        }
+
+        Book build() {
+            Book book = new Book(this);
+            return book;
+        }
+    }
+
+    Integer getId() {
+        return id;
+    }
+
+    String getTitle() {
+        return title;
+    }
+
+    String getAuthor() {
+        return author;
+    }
+
+    Integer getYear() {
+        return year;
+    }
+
+    String getLentBy() {
+        return lentBy;
+    }
+
+    void setLentBy(String lentBy) {
+        this.lentBy = lentBy;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", year=" + year +
+                ", lent by='" + lentBy + '\'' +
+                '}';
+    }
+}
